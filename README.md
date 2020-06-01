903715368ef9bbe329d466e55c9c6560f5544236
Vladimir Iashin <vdyashin@gmail.com>
2020-06-01 21:07:54 +0300

    init commit

---
+++ README.md
@@ -0,0 +1,54 @@
+# Multi-GPU Extraction of I3D Features
+
+This is a PyTorch module which does I3D feature extraction in parallel and can be scaled to any number of GPUs. The I3D paper is available here: [Quo Vadis, Action Recognition? A New Model and the Kinetics Dataset](https://arxiv.org/abs/1705.07750). 
+
+Please note, it uses PWC-Net instead of the TVL1 algorithm, which was used in the original I3D paper as it is much faster. Yet, one may create a Pool Request implementing TVL1 as an option to form optical flow frames.
+
+## Quick Start
+
+### Set up the Environment
+Setup `conda` environment. Requirements are in file `conda_env_i3d.yml`
+```bash
+# it will create new conda environment called 'i3d' on your machine 
+conda env create -f conda_env_i3d.yml
+conda activate i3d
+```
+
+### Run an Example
+It will extract I3D features and print the resulting features for `./sample` videos using 0th and 2nd devices in parallel. The features are going to be extracted with default parameters. Check out `python extrac_i3d.py --help` for help on available options.
+```bash
+python main.py --device_ids 0 2 --video_paths ./sample/v_ZNVhz7ctTq0.mp4 ./sample/v_GGSY1Qvo990.mp4
+```
+
+## More Examples
+
+The video paths can be specified in as a file with paths
+```bash
+python main.py --device_ids 0 2 --file_with_video_paths ./sample/sample_video_paths.txt
+```
+
+The features can be saved as numpy arrays by specifying `--on_extraction save_numpy`. By default, it will create a folder `./output` and will store features there.
+```bash
+python main.py --device_ids 0 2 --on_extraction save_numpy --file_with_video_paths ./sample/sample_video_paths.txt
+```
+You can change the output folder using `--output_path`.
+
+Also, you may want to try to change I3D window and step sizes. 
+```bash
+python main.py --device_ids 0 2 --stack_size 24 --step_size 24 --file_with_video_paths ./sample/sample_video_paths.txt
+```
+
+By default, the frames are extracted according to the original fps of a video. If you would like to extract frames at a certain fps, specify `--extraction_fps` argument.
+```bash
+python main.py --device_ids 0 2 --extraction_fps 25 --stack_size 24 --step_size 24 --file_with_video_paths ./sample/sample_video_paths.txt
+```
+
+Be careful with the `--keep_frames` argument when using `--extraction_fps` as it may mess up the kept frames. If `--keep_frames` is specified, it keeps them in `--tmp_path` (`./tmp` by default).
+
+
+## License 
+All is MIT except for PWC Net implementation in I3D. If you are using PWC Net (in I3D extraction, for example), please read this [License](https://github.com/sniklaus/pytorch-pwc) (Last time I checked it was _GPL-3.0_).
+
+## Credits
+1. An implementation of PWC-Net in PyTorch: https://github.com/sniklaus/pytorch-pwc
+2. A port of I3D weights from TensorFlow to PyTorch: https://github.com/hassony2/kinetics_i3d_pytorch
---
+++ main.py
@@ -0,0 +1,69 @@
+import torch
+import argparse
+
+from i3d.utils.utils import form_list_from_user_input
+from i3d.extract_i3d import ExtractI3D
+
+
+def parallel_feature_extraction(args: argparse.Namespace, feature_name: str = 'i3d'):
+    '''Distributes the feature extraction in a embarasingly-parallel fashion. Specifically,
+    it divides the dataset (list of video paths) among all specified devices evenly.
+
+    Arguments:
+        args {argparse.Namespace} -- user-defined arguments
+
+    Keyword Arguments:
+        feature_name {str} (default: {'i3d'})
+    '''
+    if args.on_extraction == 'save_numpy':
+        print(f'Saving features to {args.output_path}')
+    if args.keep_frames:
+        print(f'Saving frames to {args.tmp_path}')
+
+    if feature_name == 'i3d':
+        extractor = ExtractI3D(args)
+
+    # the indices correspond to the positions of the target videos in
+    # the video_paths list. They are required here because
+    # scatter module inputs only tensors but there is no such torch tensor
+    # that would be suitable for strings (video_paths). Also, the
+    # input have the method '.device' which allows us to access the
+    # current device in the extractor.
+    video_paths = form_list_from_user_input(args)
+    indices = torch.arange(len(video_paths))
+    replicas = torch.nn.parallel.replicate(extractor, args.device_ids[:len(indices)])
+    inputs = torch.nn.parallel.scatter(indices, args.device_ids[:len(indices)])
+    torch.nn.parallel.parallel_apply(replicas[:len(inputs)], inputs)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Extract Features')
+    # Main args
+    parser.add_argument('--video_paths', nargs='+', help='space-separated paths to videos')
+    parser.add_argument('--file_with_video_paths', help='.txt file where each line is a path')
+    parser.add_argument('--device_ids', type=int, nargs='+', help='space-separated device ids')
+    parser.add_argument('--feature_type', default='i3d', choices=['i3d'])
+    parser.add_argument('--tmp_path', default='./tmp',
+                        help='folder to store the extracted frames before the extraction')
+    parser.add_argument('--keep_frames', dest='keep_frames', action='store_true', default=False,
+                        help='to keep frames after feature extraction')
+    parser.add_argument('--on_extraction', default='print', choices=['print', 'save_numpy'],
+                        help='what to do once the stack is extracted')
+    parser.add_argument('--output_path', default='./output', help='where to store results if saved')
+    # I3D options
+    parser.add_argument('--pwc_path', default='./i3d/checkpoints/pwc_net.pt')
+    parser.add_argument('--i3d_rgb_path', default='./i3d/checkpoints/i3d_rgb.pt')
+    parser.add_argument('--i3d_flow_path', default='./i3d/checkpoints/i3d_flow.pt')
+    parser.add_argument('--min_side_size', type=int, default=256, help='min(HEIGHT, WIDTH)')
+    parser.add_argument('--extraction_fps', type=int, help='Do not specify for original video fps')
+    parser.add_argument('--stack_size', type=int, default=64, help='Feature time span in fps')
+    parser.add_argument('--step_size', type=int, default=64, help='Feature step size in fps')
+    parser.add_argument(
+        '--show_kinetics_pred', dest='show_kinetics_pred', action='store_true', default=False,
+        help='to show the predictions of the i3d model into kinetics classes for each feature'
+    )
+    parser.add_argument('--kinetics_class_labels', default='./checkpoints/label_map.txt')
+
+    args = parser.parse_args()
+
+    parallel_feature_extraction(args)
