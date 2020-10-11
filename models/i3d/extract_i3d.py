ddd3c18db1ad6b5f6f931a462baa5d869792d12c
Vladimir Iashin <vdyashin@gmail.com>
2020-10-11 16:35:24 +0300

    i3d: cf and simplified the code; global: imp print

--- models/i3d/extract_i3d.py
+++ models/i3d/extract_i3d.py
@@ -25,17 +25,20 @@ CENTRAL_CROP_MIN_SIDE_SIZE = 224
 DEFAULT_I3D_STEP_SIZE = 64
 DEFAULT_I3D_STACK_SIZE = 64
 I3D_FEATURE_TYPE = 'separately_rgb_flow'
+I3D_CLASSES_NUM = 400
 
 class ExtractI3D(torch.nn.Module):
 
     def __init__(self, args):
         super(ExtractI3D, self).__init__()
         self.feature_type = args.feature_type
-        self.streams = args.streams
+        if args.streams is None:
+            self.streams = ['rgb', 'flow']
+        else:
+            self.streams = args.streams
         self.path_list = form_list_from_user_input(args)
         self.pwc_path = PWC_PATH
-        self.rgb_weights_path = I3D_RGB_PATH
-        self.flow_weights_path = I3D_FLOW_PATH
+        self.i3d_weights_paths = {'rgb': I3D_RGB_PATH, 'flow': I3D_FLOW_PATH}
         self.min_side_size = PRE_CENTRAL_CROP_MIN_SIDE_SIZE
         self.central_crop_size = CENTRAL_CROP_MIN_SIDE_SIZE
         self.extraction_fps = args.extraction_fps
@@ -50,19 +53,22 @@ class ExtractI3D(torch.nn.Module):
             Resize(self.min_side_size),
             ToFCHW()
         ])
-        self.i3d_rgb_transforms = transforms.Compose([
-            TensorCenterCrop(self.central_crop_size),
-            ScaleTo1_1(),
-            PermuteAndUnsqueeze()
-        ])
-        self.i3d_flow_transforms = transforms.Compose([
-            TensorCenterCrop(self.central_crop_size),
-            Clamp(-20, 20),
-            ToUInt8(),
-            ScaleTo1_1(),
-            PermuteAndUnsqueeze()
-        ])
+        self.i3d_transforms = {
+            'rgb': transforms.Compose([
+                TensorCenterCrop(self.central_crop_size),
+                ScaleTo1_1(),
+                PermuteAndUnsqueeze()
+            ]),
+            'flow': transforms.Compose([
+                TensorCenterCrop(self.central_crop_size),
+                Clamp(-20, 20),
+                ToUInt8(),
+                ScaleTo1_1(),
+                PermuteAndUnsqueeze()
+            ])
+        }
         self.show_pred = args.show_pred
+        self.i3d_classes_num = I3D_CLASSES_NUM
         self.keep_tmp_files = args.keep_tmp_files
         self.on_extraction = args.on_extraction
         self.tmp_path = os.path.join(args.tmp_path, self.feature_type)
@@ -76,23 +82,16 @@ class ExtractI3D(torch.nn.Module):
         '''
         device = indices.device
 
-        pwc_model = PWCNet(self.pwc_path).to(device)
-        # i3d_model = I3D_RGB_FLOW(self.i3d_rgb_path, self.i3d_flow_path).to(device)
-        i3d_rgb_model = None
-        i3d_flow_model = None
-        if 'rgb' in self.streams:
-            i3d_rgb_model = I3D(num_classes=400, modality='rgb').to(device)
-            i3d_rgb_model.load_state_dict(torch.load(self.rgb_weights_path))
-            i3d_rgb_model.eval()
-        if 'flow' in self.streams:
-            i3d_flow_model = I3D(num_classes=400, modality='flow').to(device)
-            i3d_flow_model.load_state_dict(torch.load(self.flow_weights_path))
-            i3d_flow_model.eval()
+        flow_xtr_model = PWCNet(self.pwc_path).to(device)
+        models = {}
+        for stream in self.streams:
+            models[stream] = I3D(num_classes=self.i3d_classes_num, modality=stream).to(device).eval()
+            models[stream].load_state_dict(torch.load(self.i3d_weights_paths[stream]))
 
         for idx in indices:
             # when error occurs might fail silently when run from torch data parallel
             try:
-                feats_dict = self.extract(device, pwc_model, i3d_rgb_model, i3d_flow_model, self.path_list[idx])
+                feats_dict = self.extract(device, flow_xtr_model, models, self.path_list[idx])
                 action_on_extraction(feats_dict, self.path_list[idx], self.output_path, self.on_extraction)
             except KeyboardInterrupt:
                 raise KeyboardInterrupt
@@ -104,16 +103,15 @@ class ExtractI3D(torch.nn.Module):
             # update tqdm progress bar
             self.progress.update()
 
-    def extract(self, device: torch.device, pwc_model: torch.nn.Module,
-                i3d_rgb_model: torch.nn.Module, i3d_flow_model: torch.nn.Module,
-                video_path: Union[str, None] = None
+    def extract(self, device: torch.device, flow_xtr_model: torch.nn.Module,
+                models: Dict[str, torch.nn.Module], video_path: Union[str, None] = None
                 ) -> Dict[str, Union[torch.nn.Module, str]]:
         '''The extraction call. Made to clean the forward call a bit.
 
         Arguments:
             device {torch.device}
-            pwc_model {torch.nn.Module}
-            i3d_model {torch.nn.Module}
+            flow_xtr_model {torch.nn.Module}
+            models {Dict[str, torch.nn.Module]}
 
         Keyword Arguments:
             video_path {Union[str, None]} -- if you would like to use import it and use it as
@@ -126,7 +124,7 @@ class ExtractI3D(torch.nn.Module):
         if self.extraction_fps is not None:
             video_path = reencode_video_with_diff_fps(video_path, self.tmp_path, self.extraction_fps)
 
-        # rgb: (f, h, w, c)
+        # loading a video rgb: (f, h, w, c)
         video_rgb, _audio, _info = read_video(video_path)
 
         # sanity check
@@ -147,28 +145,24 @@ class ExtractI3D(torch.nn.Module):
             # since we apply some transforms on rgb later, we allocate the slice into a dedicated variable
             rgb_slice = video_rgb[start_idx:end_idx]
 
-            if 'flow' in self.streams:
+            for stream in self.streams:
                 with torch.no_grad():
+                    # if i3d stream is flow, we first need to calculate optical flow, otherwise, we use rgb
                     # `end_idx-1` and `start_idx+1` because flow is calculated between f and f+1 frames
-                    flow_slice = pwc_model(rgb_slice[:-1], rgb_slice[1:], device)
-                    flow_slice = self.i3d_flow_transforms(flow_slice)
-                    # shouldn't reassign variables as flow_slice is also used in show_preds
-                    flow_feats = i3d_flow_model(flow_slice, features=True)  # (B, 1024)
-                    feats_dict['flow'].extend(flow_feats.tolist())
+                    # we also use `end_idx-1` for stream == 'rgb' case: just to make sure the feature length
+                    # is same regardless of whether only rgb is used or flow
+                    if stream == 'flow':
+                        stream_slice = flow_xtr_model(rgb_slice[:-1], rgb_slice[1:], device)
+                    elif stream == 'rgb':
+                        stream_slice = rgb_slice[:-1]
+                    else:
+                        raise NotImplementedError
+                    stream_slice = self.i3d_transforms[stream](stream_slice)
+                    feats = models[stream](stream_slice, features=True)  # (B, 1024)
+                    feats_dict[stream].extend(feats.tolist())
                     if self.show_pred:
-                        softmaxes, logits = i3d_flow_model(flow_slice, features=False)
-                        print(f'{video_path} @ stack {stack_idx} (flow stream)')
-                        show_predictions_on_dataset(logits, 'kinetics')
-
-            if 'rgb' in self.streams:
-                with torch.no_grad():
-                    rgb_slice = self.i3d_rgb_transforms(rgb_slice[:-1])
-                    # shouldn't reassign variables as rgb_slice is also used in show_preds
-                    rgb_feats = i3d_rgb_model(rgb_slice, features=True)
-                    feats_dict['rgb'].extend(rgb_feats.tolist())
-                    if self.show_pred:
-                        softmaxes, logits = i3d_rgb_model(rgb_slice, features=False)
-                        print(f'{video_path} @ stack {stack_idx} (rgb stream)')
+                        softmaxes, logits = models[stream](stream_slice, features=False)  # (B, classes=400)
+                        print(f'{video_path} @ stack {stack_idx} ({stream} stream)')
                         show_predictions_on_dataset(logits, 'kinetics')
 
         # removes the video with different fps if it was created to preserve disk space
