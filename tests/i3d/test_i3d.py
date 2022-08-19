52e475d65cbb7b49d8656ca768a92655be774595
Vladimir Iashin <vdyashin@gmail.com>
2022-08-19 15:34:28 +0300

    tests: cmd API I3D

--- tests/i3d/test_i3d.py
+++ tests/i3d/test_i3d.py
@@ -1,4 +1,8 @@
+import pickle
+import numpy as np
+import subprocess
 import sys
+import tempfile
 from pathlib import Path
 
 import pytest
@@ -9,7 +13,7 @@ sys.path.insert(0, '.')  # nopep8
 
 from models.i3d.extract_i3d import ExtractI3D as Extractor
 from tests.utils import make_ref
-from utils.utils import build_cfg_path
+from utils.utils import build_cfg_path, on_after_sanity_check, sanity_check
 
 # a bit ugly but it assumes the features being tested has the same folder name,
 # e.g. for r21d: ./tests/r21d/THIS_FILE
@@ -41,6 +45,7 @@ else:
 
 @pytest.mark.parametrize(signature, test_params)
 def test(device, video_path, streams, flow_type, stack_size, step_size, extraction_fps, to_make_ref):
+    # calculate features with the 'import' API
     # output
     args = OmegaConf.load(build_cfg_path(FEATURE_TYPE))
     args.video_paths = video_path
@@ -49,22 +54,65 @@ def test(device, video_path, streams, flow_type, stack_size, step_size, extracti
     args.stack_size = stack_size
     args.step_size = step_size
     args.extraction_fps = extraction_fps
+    sanity_check(args)
+    on_after_sanity_check(args)
+    # init the extractor
     extractor = Extractor(args)
     model, class_head = extractor.load_model(device)
-    features = extractor.extract(device, model, class_head, video_path)
-    for k in [key for key in features.keys() if key in ['rgb', 'flow']]:
-        features_out = features[k]
+    feat_out_import = extractor.extract(device, model, class_head, video_path)
+
+    # calculate features with the 'cmd' API
+    with tempfile.TemporaryDirectory(suffix='_todel_video_features') as output_root:
+        ways_to_save = ['save_numpy', 'save_pickle']
+        feat_out_cmd = {k: dict() for k in ways_to_save}
+        # we are going to test both: `save_numpy` and `save_pickle`
+        for on_extraction in ways_to_save:
+            # make a cmd (the quotation of arguments might lead to unwanted fails :/)
+            cmd = f'{sys.executable} main.py'
+            cmd += f' feature_type="{FEATURE_TYPE}"'
+            cmd += f' flow_type="{flow_type}"'
+            cmd += ' device_ids=0' if device == 'cuda' else ' cpu="true"'
+            cmd += f' stack_size={stack_size}' if stack_size else ''
+            cmd += f' step_size={step_size}' if step_size else ''
+            cmd += f' extraction_fps={extraction_fps}' if extraction_fps else ''
+            cmd += f' on_extraction="{on_extraction}"' if on_extraction else ''
+            cmd += f' video_paths={video_path}'
+            cmd += f' output_path="{output_root}"'
+            # damn, it is ugly
+            # call the cmd
+            subprocess.call(cmd.split())
+            # read from the saved file
+            for key in feat_out_import.keys():
+                # for search: `output_path`
+                # TODO: fix this as it now hard-coded by during extration it is in `on_after_sanity_check`
+                load_path_stub = Path(output_root) / FEATURE_TYPE / f'{Path(video_path).stem}_{key}'
+                if on_extraction == 'save_numpy':
+                    assert load_path_stub.with_suffix('.npy').exists(), (load_path_stub, output_root)
+                    feat = np.load(load_path_stub.with_suffix('.npy'))
+                elif on_extraction == 'save_pickle':
+                    assert load_path_stub.with_suffix('.pkl').exists()
+                    feat = pickle.load(open(load_path_stub.with_suffix('.pkl'), 'rb'))
+                feat_out_cmd[on_extraction][key] = feat
+
+
+    for k in [key for key in feat_out_import.keys() if key in ['rgb', 'flow']]:
+        # compare features saved by pickle and numpy
+        assert (feat_out_cmd['save_pickle'][key] - feat_out_cmd['save_numpy'][key]).sum() < 1e-6
+        # compare cmd API and import API. Assuming if it passes these tests, only `feat_out` will be used
+        assert (feat_out_cmd['save_numpy'][key] - feat_out_import[key]).sum() < 1e-6
+
+        feat_out = feat_out_import[k]
         # load/make the reference (make ref will make the reference file which will be used to compare with)
         filename = f'{device}_{Path(video_path).stem}_{streams}_{flow_type}_{stack_size}_{step_size}_{extraction_fps}_{k}.pt'
         ref_path = Path('./tests') / FEATURE_TYPE / 'reference' / filename
         if to_make_ref:
-            make_ref(args, video_path, features_out, ref_path)
-        features_ref = torch.load(ref_path)['data']
+            make_ref(args, video_path, feat_out, ref_path)
+        feat_ref = torch.load(ref_path)['data']
         # tests
         print(k)
-        print(features_out)
-        print(features_out.shape)
-        print(features_ref)
-        print(features_ref.shape)
-        assert features_out.shape == features_ref.shape
-        assert (features_out - features_ref).sum() < 1e-6
+        print(feat_out)
+        print(feat_out.shape)
+        print(feat_ref)
+        print(feat_ref.shape)
+        assert feat_out.shape == feat_ref.shape
+        assert (feat_out - feat_ref).sum() < 1e-6
