c22f49bcc5ccb80f2189242de51a995d40dc924c
Vladimir Iashin <vdyashin@gmail.com>
2024-01-21 21:27:04 +0000

    Updated dependencies (#115)
    * resnet: new torchvision API
    * step-by-step installation of conda
    * r21d: new torchvision API
    * raft: fix warning caused by new version of PyTorch
    * vggish: values are quite different from the before
    * upd conda env: newer pt, av, and ffmpeg, opencv

---
+++ docs/meta/install_conda.md
@@ -0,0 +1,10 @@
+Just steps to install conda and create a new environment from scratch.
+```bash
+conda create -n video_features
+conda install pytorch torchvision torchaudio pytorch-cuda=12.1 -c pytorch -c nvidia
+conda install -c conda-forge omegaconf scipy tqdm pytest opencv
+# +CLIP
+conda install -c conda-forge ftfy regex
+# vggish
+conda install -c conda-forge resampy pysoundfile
+```
--- models/raft/raft_src/corr.py
+++ models/raft/raft_src/corr.py
@@ -36,7 +36,7 @@ class CorrBlock:
             corr = self.corr_pyramid[i]
             dx = torch.linspace(-r, r, 2 * r + 1)
             dy = torch.linspace(-r, r, 2 * r + 1)
-            delta = torch.stack(torch.meshgrid(dy, dx), axis=-1).to(coords.device)
+            delta = torch.stack(torch.meshgrid(dy, dx, indexing='ij'), axis=-1).to(coords.device)
 
             centroid_lvl = coords.reshape(batch * h1 * w1, 1, 1, 2) / 2 ** i
             delta_lvl = delta.view(1, 2 * r + 1, 2 * r + 1, 2)
